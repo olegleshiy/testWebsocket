1cbd796bd5d0a0575eb976e6aeac74775fd3f5f8
olegleshiy <olegleshiy7@gmail.com>
2019-10-06 23:14:32 +0300

    add code

---
+++ public/app.js
@@ -0,0 +1,48 @@
+const status = document.getElementById('status');
+const messages = document.getElementById('messages');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const URL = 'http://api.openweathermap.org/data/2.5/weather?id=703448&appid=77b3a59577b93fe4e2be80940c52db12';
+
+const ws = new WebSocket('ws://localhost:3012');
+
+function setStatus(value) {
+    status.innerHTML = value;
+}
+
+function printMessage(value) {
+    const li = document.createElement('li');
+
+    li.innerHTML = value;
+    messages.appendChild(li);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    ws.send(input.value);
+    input.value = '';
+});
+
+ws.onopen = () => setStatus('ONLINE');
+
+ws.onclose = () => setStatus('DISCONNECTED');
+
+ws.onmessage = response => printMessage(response.data);
+
+setInterval(() => {
+    fetch(URL)
+        .then(resp => resp.json())
+        .then((data) => {
+            document.querySelector('.city-name').textContent = data.name;
+            document.querySelector('.temp').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
+            document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
+            document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`
+        })
+        .then(() => {
+            clearInterval(update);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+}, 3600);
---
+++ server.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 3012 });
+
+server.on('connection', ws => {
+    ws.on('message', message => {
+        if(message === 'exit') {
+            ws.close();
+        } else {
+            server.clients.forEach(client => {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(message);
+                }
+            });
+        }
+
+    });
+    ws.send('Добро пожаловать');
+    ws.on('close', (reasonCode, description) => {
+        console.log('Disconnected ' + ws.remoteAddress);
+        console.dir({ reasonCode, description });
+    });
+});
